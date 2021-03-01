2d35c84cf25d5b61a6df3fed471eed8fa1cc4857
Tomáš Šesták <tomassestak@seznam.cz>
2021-03-01 14:59:13 +0100

    Create useScrollToElement.js

---
+++ useScrollToElement.js
@@ -0,0 +1,34 @@
+import {useSpring} from 'react-spring'
+
+export const useScrollToElement = () => {
+	const [, setY] = useSpring(() => ({ y: 0 }))
+
+	let isStopped = false
+	const onWheel = () => {
+		isStopped = true
+		window.removeEventListener('wheel', onWheel)
+	}
+
+	const scrollToTarget = (node) => {
+		const value: number = window.scrollY + node.getBoundingClientRect().top
+
+		window.addEventListener('wheel', onWheel)
+
+		setY({
+			y: value,
+			reset: true,
+			from: { y: window.scrollY },
+			onRest: () => {
+				isStopped = false
+				window.removeEventListener('wheel', onWheel)
+			},
+			onChange: ({y}) => {
+				if (!isStopped) {
+					window.scroll(0, y)
+				}
+			}
+		})
+	}
+
+	return {scrollToTarget}
+}
