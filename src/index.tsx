cb9d9592bb25c82b8b1baea1d76ebb8bc60bc332
Mihailo Cvetkovic <mihailo@yolkstudio.com>
2021-05-21 23:32:00 +0200

    default scroll offset value = 0

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,46 @@
 import {SpringConfig, useSpring} from '@react-spring/web';
 
-export default function(config?: SpringConfig) {
-	const [, api] = useSpring(() => ({ y: 0 }));
+export default function (config?: SpringConfig) {
+    const [, api] = useSpring(() => ({y: 0}));
 
-	let isStopped = false;
-	const onWheel = () => {
-		isStopped = true;
-		window.removeEventListener('wheel', onWheel);
-	};
+    let isStopped = false;
+    const onWheel = () => {
+        isStopped = true;
+        window.removeEventListener('wheel', onWheel);
+    };
 
-	const scrollTo = (value?: Element | number | string | null, offset?: number) => {
-		let y = 0;
+    const scrollTo = (value?: Element | number | string | null, offset: number = 0) => {
+        let y = 0;
 
-		if (typeof value === 'number') {
-			y = value;
-		}  else if (typeof value === 'string') {
-			value = document.querySelector(value);
-			if (value) {
-				y = window.scrollY + value.getBoundingClientRect().top + (offset ?? 0)
-			}
-		} else if (value?.nodeType === 1) {
-			y = window.scrollY + value.getBoundingClientRect().top + (offset ?? 0);
-		}
+        if (typeof value === 'number') {
+            y = value;
+        } else if (typeof value === 'string') {
+            value = document.querySelector(value);
+            if (value) {
+                y = window.scrollY + value.getBoundingClientRect().top + offset;
+            }
+        } else if (value?.nodeType === 1) {
+            y = window.scrollY + value.getBoundingClientRect().top + offset;
+        }
 
-		window.addEventListener('wheel', onWheel);
+        window.addEventListener('wheel', onWheel);
 
-		api.start({
-			y,
-			reset: true,
-			from: { y: window.scrollY },
-			config,
-			onRest: () => {
-				isStopped = false;
-				window.removeEventListener('wheel', onWheel);
-			},
-			onChange: (_, ctrl) => {
-				if (!isStopped) {
-					window.scroll(0, ctrl.get().y);
-				}
-			},
-		});
-	};
+        api.start({
+            y,
+            reset: true,
+            from: {y: window.scrollY},
+            config,
+            onRest: () => {
+                isStopped = false;
+                window.removeEventListener('wheel', onWheel);
+            },
+            onChange: (_, ctrl) => {
+                if (!isStopped) {
+                    window.scroll(0, ctrl.get().y);
+                }
+            },
+        });
+    };
 
-	return { scrollTo };
+    return {scrollTo};
 };
