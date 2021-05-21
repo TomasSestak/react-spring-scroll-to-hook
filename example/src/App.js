c7d1c17f8f24991d79643c8c70f04b4aaef0abfe
Tomáš Šesták <tomassestak@seznam.cz>
2021-05-21 22:29:53 +0200

    improve example

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import {config} from '@react-spring/web';
 import useScrollTo from 'react-spring-scroll-to-hook'
 
 const App = () => {
-	const {scrollTo} = useScrollTo(config.molasses)
+	const {scrollTo} = useScrollTo()
 	return (
 		<>
 			<header id={'id'}/>
-			<button type="button" onClick={() => scrollTo('#id', 20)}>
+			<button type="button" onClick={scrollTo}>
 				Click me!
 			</button>
 		</>
